fbf8e62151c709b66ef258eed60ded91f3aeae2d
Rithsagea <rithsagea@gmail.com>
2025-07-30 15:57:06 -0400

    clean up serialization with strategy

--- src/util/Data.test.ts
+++ src/util/Data.test.ts
@@ -1,6 +1,19 @@
 import { expect, test } from "bun:test";
 import { deserialize, Property, serialize, type Context } from "./Data";
 import { BiMap } from "./Algorithms";
+import type { Constructor } from "./Types";
+
+function testSerialize<T extends object>(
+  source: T,
+  target: any,
+  constructor: Constructor<T>,
+) {
+  const forwardResult = serialize(source);
+  expect(forwardResult).toStrictEqual(target);
+
+  const backwardResult = deserialize(target, constructor);
+  expect(backwardResult).toStrictEqual(source);
+}
 
 class Struct {
   @Property.Primitive stringField: string = "";
@@ -10,34 +23,16 @@ class Struct {
 }
 
 test("serialize primitive field", () => {
-  const struct = new Struct();
-  struct.stringField = "foo";
-  struct.numberField = 39;
-  struct.ignoredField = true;
+  const source = new Struct();
+  source.stringField = "foo";
+  source.numberField = 39;
 
-  const expected = {
+  const target = {
     stringField: "foo",
     numberField: 39,
   };
 
-  const result = serialize(struct);
-
-  expect(result).toStrictEqual(expected);
-});
-
-test("deserialize primitive field", () => {
-  const data = {
-    stringField: "foo",
-    numberField: 39,
-  };
-
-  const expected = new Struct();
-  expected.stringField = "foo";
-  expected.numberField = 39;
-
-  const result = deserialize(data, Struct);
-
-  expect(result).toStrictEqual(expected);
+  testSerialize(source, target, Struct);
 });
 
 class Node {
@@ -46,75 +41,31 @@ class Node {
 }
 
 test("serialize nested object", () => {
-  const root = new Node();
-  root.value = 1;
-  root.next = new Node();
-  root.next.value = 2;
+  const source = new Node();
+  source.value = 1;
+  source.next = new Node();
+  source.next.value = 2;
 
-  const expected = {
+  const target = {
     value: 1,
     next: { value: 2 },
   };
 
-  const result = serialize(root);
-
-  expect(result).toStrictEqual(expected);
-});
-
-test("deserialize nested object", () => {
-  const data = {
-    value: 1,
-    next: { value: 2 },
-  };
-
-  const expected = new Node();
-  expected.value = 1;
-  expected.next = new Node();
-  expected.next.value = 2;
-
-  const result = deserialize(data, Node);
-
-  expect(result).toStrictEqual(expected);
-});
-
-class NodeContainer {
-  @Property.Class(Node) nodes: Node[] = [];
-}
-
-test("serialize list", () => {
-  const indices = [1, 2, 3, 4, 5];
-  const container = new NodeContainer();
-  container.nodes = indices.map((i) => {
-    const node = new Node();
-    node.value = i;
-    return node;
-  });
-
-  const expected = { nodes: indices.map((i) => ({ value: i })) };
-  const result = serialize(container);
-  expect(result).toStrictEqual(expected);
-});
-
-test("deserialize list", () => {
-  const indices = [1, 2, 3, 4, 5];
-  const data = { nodes: indices.map((i) => ({ value: i })) };
-
-  const expected = new NodeContainer();
-  expected.nodes = indices.map((i) => {
-    const node = new Node();
-    node.value = i;
-    return node;
-  });
-  const result = deserialize(data, NodeContainer);
-  expect(result).toStrictEqual(expected);
+  testSerialize(source, target, Node);
 });
 
 class StringBox {
-  @Property.Primitive value: string = "";
+  @Property.Primitive value: string;
+  constructor(value: string = "") {
+    this.value = value;
+  }
 }
 
 class NumberBox {
-  @Property.Primitive value: number = 0;
+  @Property.Primitive value: number;
+  constructor(value: number = 0) {
+    this.value = value;
+  }
 }
 
 const context: Context = BiMap.from({
@@ -127,33 +78,57 @@ class Container {
 }
 
 test("serialize subclass", () => {
-  const container = new Container();
+  const source = new Container();
 
-  container.box = new StringBox();
-  container.box.value = "foo";
-  const expectedStringBox = { $type: "string", box: { value: "foo" } };
-  const resultString = serialize(container);
-  expect(resultString).toStrictEqual(expectedStringBox);
+  source.box = new StringBox("foo");
+  const targetStringBox = { box: { $type: "string", value: "foo" } };
+  testSerialize(source, targetStringBox, Container);
 
-  container.box = new NumberBox();
-  container.box.value = 39;
-  const expectedNumberBox = { $type: "number", box: { value: 39 } };
-  const resultNumber = serialize(container);
-  expect(resultNumber).toStrictEqual(expectedNumberBox);
+  source.box = new NumberBox(39);
+  const targetNumberBox = { box: { $type: "number", value: 39 } };
+  testSerialize(source, targetNumberBox, Container);
 });
 
-test("deserialize subclass", () => {
-  const expected = new Container();
+class ListContainer {
+  @Property.List(context) list: (StringBox | NumberBox)[] = [];
+}
 
-  const stringBox = { $type: "string", box: { value: "foo" } };
-  const resultStringBox = deserialize(stringBox, Container);
-  expected.box = new StringBox();
-  expected.box.value = "foo";
-  expect(resultStringBox).toStrictEqual(expected);
+test("serialize list", () => {
+  const source = new ListContainer();
+  source.list = [
+    ...[1, 2, 3].map((i) => new NumberBox(i)),
+    ...["a", "b", "c"].map((i) => new StringBox(i)),
+  ];
 
-  const numberBox = { $type: "number", box: { value: 39 } };
-  const resultNumberBox = deserialize(numberBox, Container);
-  expected.box = new NumberBox();
-  expected.box.value = 39;
-  expect(resultNumberBox).toStrictEqual(expected);
+  const target = {
+    list: [
+      ...[1, 2, 3].map((i) => ({ $type: "number", value: i })),
+      ...["a", "b", "c"].map((i) => ({ $type: "string", value: i })),
+    ],
+  };
+
+  testSerialize(source, target, ListContainer);
+});
+
+class MapContainer {
+  @Property.Map(context) map: Record<string, StringBox | NumberBox> = {};
+}
+
+test("serialize map", () => {
+  const source = new MapContainer();
+  source.map.a = new StringBox("a");
+  source.map.b = new StringBox("b");
+  source.map.c = new NumberBox(1);
+  source.map.d = new NumberBox(2);
+
+  const target = {
+    map: {
+      a: { $type: "string", value: "a" },
+      b: { $type: "string", value: "b" },
+      c: { $type: "number", value: 1 },
+      d: { $type: "number", value: 2 },
+    },
+  };
+
+  testSerialize(source, target, MapContainer);
 });
--- src/util/Data.ts
+++ src/util/Data.ts
@@ -1,75 +1,58 @@
+import assert from "assert";
 import type { BiMap } from "./Algorithms";
 import { getMetadata } from "./Metadata";
 import { type Constructor } from "./Types";
 
 export type Context = BiMap<string, Constructor>;
 
-interface PrimitivePropertyEntry {
-  type: "primitive";
-}
-
-interface ClassPropertyEntry {
-  type: "class";
-  constructor: Constructor;
-}
-
-interface MultiPropertyEntry {
-  type: "multi";
-  context: Context;
-}
-
-type PropertyEntry =
-  | PrimitivePropertyEntry
-  | ClassPropertyEntry
-  | MultiPropertyEntry;
-
-// TODO: update value type to method
-type PropertyData = Record<string, PropertyEntry>;
+type PropertyData = Record<string, SerializationStrategy<any>>;
 const SUBSCRIPTION_SYMBOL = Symbol("properties");
 function getPropertyData(target: object) {
   return getMetadata<PropertyData>(target, SUBSCRIPTION_SYMBOL, {});
 }
 
 export namespace Property {
+  export function Serialize(strategy: SerializationStrategy) {
+    return (target: object, key: string) => {
+      getPropertyData(target)[key] = strategy;
+    };
+  }
+
   export function Primitive(target: object, key: string) {
-    getPropertyData(target)[key] = { type: "primitive" };
+    getPropertyData(target)[key] = PrimitiveSerializationStrategy();
   }
 
   export function Class<T extends object>(constructor: Constructor<T>) {
-    return (target: object, key: string) => {
-      getPropertyData(target)[key] = { type: "class", constructor };
-    };
+    return Serialize(ClassSerializationStrategy(constructor));
   }
 
   export function Multi(context: Context) {
-    return (target: object, key: string) => {
-      getPropertyData(target)[key] = { type: "multi", context };
-    };
+    return Serialize(MultiSerializationStrategy(context));
+  }
+
+  function getStrategy(context?: Constructor | Context) {
+    if (context === undefined) return PrimitiveSerializationStrategy();
+    else if (typeof context === "function")
+      return ClassSerializationStrategy(context);
+    else return MultiSerializationStrategy(context);
+  }
+
+  export function List(context?: Constructor | Context) {
+    return Serialize(ListSerializationStrategy(getStrategy(context)));
+  }
+
+  export function Map(context?: Constructor | Context) {
+    return Serialize(MapSerializationStrategy(getStrategy(context)));
   }
 }
 
 export function serialize(target: object): object {
-  if (Array.isArray(target)) return target.map(serialize);
-
   const res: any = {};
   const properties = getPropertyData(target);
   for (const [label, property] of Object.entries(properties)) {
     const value = (target as any)[label];
     if (value === undefined) continue;
-    switch (property.type) {
-      case "primitive":
-        res[label] = value;
-        break;
-      case "class":
-        res[label] = serialize(value);
-        break;
-      case "multi":
-        res[label] = serialize(value);
-        res.$type = property.context.inverse().get(value.constructor);
-        break;
-      default:
-        throw Error(`Unsupported property ${property}`);
-    }
+    res[label] = property.serialize(value);
   }
 
   return res;
@@ -79,31 +62,82 @@ export function deserialize<T extends object>(
   source: any,
   constructor: Constructor<T>,
 ): T {
-  if (Array.isArray(source))
-    return source.map((e) => deserialize(e, constructor)) as any;
-  // TODO: fix typing here!
-
   const res = new constructor();
   const properties = getPropertyData(res);
   for (const [label, property] of Object.entries(properties)) {
     const value = (source as any)[label];
     if (value === undefined) continue;
-    switch (property.type) {
-      case "primitive":
-        (res as any)[label] = value;
-        break;
-      case "class":
-        (res as any)[label] = deserialize(value, property.constructor);
-        break;
-      case "multi":
-        (res as any)[label] = deserialize(
-          value,
-          property.context.get(source.$type)!,
-        );
-        break;
-      default:
-        throw Error(`Unsupported property ${property}`);
-    }
+    (res as any)[label] = property.deserialize(value);
   }
   return res;
+}
+
+interface SerializationStrategy<T = any> {
+  serialize(source: T): any;
+  deserialize(source: object): T;
+}
+
+function PrimitiveSerializationStrategy(): SerializationStrategy {
+  return {
+    serialize: (i) => i,
+    deserialize: (i) => i,
+  };
+}
+
+function ClassSerializationStrategy<T extends object>(
+  constructor: Constructor<T>,
+): SerializationStrategy<T> {
+  return {
+    serialize(source) {
+      return serialize(source);
+    },
+    deserialize(source) {
+      return deserialize(source, constructor);
+    },
+  };
+}
+
+function MultiSerializationStrategy(context: Context): SerializationStrategy {
+  return {
+    serialize(source) {
+      return {
+        ...serialize(source),
+        $type: context.inverse().get(source.constructor),
+      };
+    },
+    deserialize(source) {
+      return deserialize(source, context.get((source as any).$type)!);
+    },
+  };
+}
+
+function ListSerializationStrategy<T>(
+  base: SerializationStrategy<T>,
+): SerializationStrategy<T[]> {
+  return {
+    serialize(source) {
+      return source.map(base.serialize);
+    },
+    deserialize(source) {
+      assert(Array.isArray(source));
+      return source.map(base.deserialize);
+    },
+  };
+}
+
+function MapSerializationStrategy<T>(
+  base: SerializationStrategy<T>,
+): SerializationStrategy<Record<string, T>> {
+  return {
+    serialize(source) {
+      const res: any = {};
+      for (const k in source) res[k] = base.serialize(source[k]!);
+      return res;
+    },
+    deserialize(source) {
+      const res: Record<string, T> = {};
+      for (const k in source) res[k] = base.deserialize((source as any)[k]);
+      return res;
+    },
+  };
 }
